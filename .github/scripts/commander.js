14ed5010deca4094ab120d6b30bc4e71404c43f4
Zao Soula <contact@zaosoula.fr>
2022-03-10 15:46:59 +0100

    Update commander.js

--- .github/scripts/commander.js
+++ .github/scripts/commander.js
@@ -1,10 +1,27 @@
+const parseCommand = (text) => text.match(/^\/([\w]+)\b *(.*)?$/m);
+
 module.exports = async ({github, context, core}) => {
+
+  const { debug, setFailed } = core;
+
   console.log(context);
 
-  github.issues.createComment({
-    issue_number: context.issue.number,
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    body: '![Ping Pong](https://media.giphy.com/media/l41lIvPtFdU3cLQjK/giphy.gif)',
-  }); 
+  debug("Getting the comment and checking it for a command");
+
+  const comment = context.payload.comment;
+  const command = parseCommand(comment.body);
+
+  if(!command) {
+    setFailed('No command detected');
+    return false;
+  }
+
+  debug('Command detected');
+  console.log(command);
+  // github.issues.createComment({
+  //   issue_number: context.issue.number,
+  //   owner: context.repo.owner,
+  //   repo: context.repo.repo,
+  //   body: '![Ping Pong](https://media.giphy.com/media/l41lIvPtFdU3cLQjK/giphy.gif)',
+  // }); 
 }
