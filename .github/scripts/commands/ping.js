b4faf59cf559bf104bb3ef0f95ca8b489b62a766
Zao Soula <contact@zaosoula.fr>
2022-03-10 17:03:40 +0100

    Update ping.js

--- .github/scripts/commands/ping.js
+++ .github/scripts/commands/ping.js
@@ -2,7 +2,7 @@ const { useHelpers, Reactions } = require('./helpers');
 
 module.export = {
   command: 'ping',
-  execute: ({github, context, core}) => {
+  execute: async ({github, context, core}) => {
     const { addComment, addReaction } = useHelpers({github, context, core});
 
     await addComment('![Ping Pong](https://media.giphy.com/media/l41lIvPtFdU3cLQjK/giphy.gif)');
